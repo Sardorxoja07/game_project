097cd3e8cb741876a021a35e753487b4a5630164
Sardorxoja07 <105536826+Sardorxoja07@users.noreply.github.com>
2022-08-22 10:09:38 +0500

    Add files via upload

---
+++ game_project/jss/index.js
@@ -0,0 +1,50 @@
+const word_list = [
+    {
+        word: "tashkent",
+        hint: "Where is the capital of Uzbekistan?"
+    },
+    {
+        word: "uzbekistan",
+        hint: "Where was Al Bukhari born?"
+    },
+    {
+        word: "washington",
+        hint: "Where is the capital of america"
+    },
+    {
+        word: "uzbekistan",
+        hint: "Where was Amir Timur from?"
+    },
+    {
+        word: "delhi",
+        hint: "Where is the capital of india"
+    },
+    {
+        word: "india",
+        hint: "Zahiruddin Muhammad Babur was the king of the place"
+    },
+    {
+        word: "argentina",
+        hint: "Where is Leonel Messi from"
+    },
+    {
+        word: "america",
+        hint: "Where is boxer Muhammad Ali from?"
+    },
+    {
+        word: "stivJobs",
+        hint: "Who founded Apple?"
+    },
+    {
+        word: "deep",
+        hint: "The more you get from it, the bigger it gets. What is it?"
+    },
+    {
+        word: "Herat",
+        hint: "Where was Alisher Navoi born?"
+    },
+    {
+        word: "paris",
+        hint: "Where is the eiffel tower located"
+    },
+]
